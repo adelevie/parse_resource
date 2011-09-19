6851959cdf700c664105bf08c2f0ae2cd331a6a6
Alan deLevie <adelevie@gmail.com>
2011-09-18 21:08:50 -0400

    stub for subclass test

---
+++ test/test_subclass.rb
@@ -0,0 +1,15 @@
+require 'helper'
+
+class Post < ParseResource
+
+end
+
+class TestParseResource < Test::Unit::TestCase
+  def setup
+    @p = ParseResouce.new
+  end
+
+  def test_foo_method
+    assert_equal @p.foo, "nothing yet to see here"
+  end
+end
