4d5750de0a5ff9bac24a56ee3f7951d09a3b8009
Alan deLevie <adelevie@gmail.com>
2011-10-09 16:08:40 -0400

    Updating docs. More to come!

--- lib/parse_resource.rb
+++ lib/parse_resource.rb
@@ -6,8 +6,6 @@ require "rest-client"
 require "json"
 require "active_support/hash_with_indifferent_access"
 
-ActiveSupport
-
 class ParseResource
   # ParseResource provides an easy way to use Ruby to interace with a Parse.com backend
   # Usage:
@@ -22,11 +20,14 @@ class ParseResource
   extend ActiveModel::Callbacks
   HashWithIndifferentAccess = ActiveSupport::HashWithIndifferentAccess
 
-  #define_model_callbacks :initialize, :find, :only => :after
+  # define_model_callbacks :initialize, :find, :only => :after
   define_model_callbacks :save, :create, :update, :destroy
 
-  # instantiation!
-  # p = Post.new(:title => "cool story")
+
+  # Instantiates a ParseResource object
+  #
+  # @params [Hash], [Boolean] a `Hash` of attributes and a `Boolean` that should be false only if the object already exists
+  # @return [ParseResource] an object that subclasses `Parseresource`
   def initialize(attributes = {}, new=true)
     attributes = HashWithIndifferentAccess.new(attributes)
     if new
@@ -40,6 +41,10 @@ class ParseResource
     create_setters!
   end
 
+  # Explicitly adds a field to the model.
+  #
+  # @param [Symbol] name the name of the field, eg `:author`.
+  # @param [Boolean] val the return value of the field. Only use this within the class.
   def self.field(name, val=nil)
     class_eval do
       define_method(name) do
@@ -53,13 +58,15 @@ class ParseResource
     end
   end
 
+  # Add multiple fields in one line. Same as `#field`, but accepts multiple args.
+  #
+  # @param [Array] *args an array of `Symbol`s, `eg :author, :body, :title`.
   def self.fields(*args)
     args.each {|f| field(f)}
   end
 
-  # a sprinkle of metaprogramming
-  # p = Post.new(:some_attr => "some value")
-  # p.some_attr = "new value"
+  # Creates getter and setter methods for model fields
+  # 
   def create_setters!
     @attributes.each_pair do |k,v|
       self.class.send(:define_method, "#{k}=") do |val|
@@ -82,13 +89,18 @@ class ParseResource
 
   class << self
     @@settings ||= nil
-
+-
+    # Explicitly set Parse.com API keys.
+    #
+    # @param [String] app_id the Application ID of your Parse database
+    # @param [String] master_key the Master Key of your Parse database
     def load!(app_id, master_key)
       @@settings = {"app_id" => app_id, "master_key" => master_key}
     end
 
-    # creates a RESTful resource
+    # Creates a RESTful resource
     # sends requests to [base_uri]/[classname]
+    #
     def resource
       if @@settings.nil?
         path = "config/parse_resource.yml"
@@ -101,33 +113,44 @@ class ParseResource
       RestClient::Resource.new(base_uri, app_id, master_key)
     end
 
-    # finders
-    # Post.find("abcdf")
+    # Find a ParseResource object by ID
+    #
+    # @param [String] id the ID of the Parse object you want to find.
+    # @return [ParseResource] an object that subclasses ParseResource.
     def find(id)
       where(:objectId => id).first
     end
 
-    # Post.where(:author => "Alan", :title => "Ipso Lorem")
+    # Find a ParseResource object by a `Hash` of conditions.
+    #
+    # @param [Hash] parameters a `Hash` of conditions.
+    # @return [Array] an `Array` of objects that subclass `ParseResource`.
     def where(parameters)
       resp = resource.get(:params => {:where => parameters.to_json})
       results = JSON.parse(resp)['results']
       results.map {|r| model_name.constantize.new(r, false)}
     end
 
-    # Post.all
+    # Find all ParseResource objects for that model.
+    #
+    # @return [Array] an `Array` of objects that subclass `ParseResource`.
     def all
       resp = resource.get
       results = JSON.parse(resp)['results']
       results.map {|r| model_name.constantize.new(r, false)}
     end
 
-    # Post.create(:title => "new post")
+    # Create a ParseResource object.
+    #
+    # @param [Hash] attributes a `Hash` of attributes
+    # @return [ParseResource] an object that subclasses `ParseResource`. Or returns `false` if object fails to save.
     def create(attributes = {})
       attributes = HashWithIndifferentAccess.new(attributes)
       new(attributes).save
     end
 
-    # Post.first
+    # Find the first object. Fairly random, not based on any specific condition.
+    #
     def first
       all.first
     end
@@ -231,14 +254,4 @@ class ParseResource
 
   def updated_at; self.updatedAt rescue nil; end
 
-  # another sprinkle of metaprogramming
-  # p = Post.new(:some_attr => "some value")
-  # p.some_attr #=> "some value"
-  #def method_missing(meth, *args, &block)
-  #  if @attributes.has_key?(meth.to_s)
-  #    @attributes[meth.to_s]
-  #  else
-  #    super
-  #  end
-  #end
 end
