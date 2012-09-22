e6be1e478de6f93812b7dc2f2e2b417b210f813b
Alan deLevie <adelevie@gmail.com>
2012-09-22 12:09:49 -0400

    moved query methods into...ParseResource::QueryMethods

--- lib/parse_resource/base.rb
+++ lib/parse_resource/base.rb
@@ -6,6 +6,7 @@ require "rest-client"
 require "json"
 require "active_support/hash_with_indifferent_access"
 require "parse_resource/query"
+require "parse_resource/query_methods"
 require "parse_resource/parse_error"
 require "parse_resource/parse_exceptions"
 
@@ -227,47 +228,9 @@ module ParseResource
       Query.new(self).where(*args)
     end
     
-    # Include the attributes of a parent ojbect in the results
-    # Similar to ActiveRecord eager loading
-    #
-    def self.include_object(parent)
-      Query.new(self).include_object(parent)
-    end
 
-    # Add this at the end of a method chain to get the count of objects, instead of an Array of objects
-    def self.count
-      #https://www.parse.com/docs/rest#queries-counting
-      Query.new(self).count(1)
-    end
+    include ParseResource::QueryMethods
 
-    # Find all ParseResource::Base objects for that model.
-    #
-    # @return [Array] an `Array` of objects that subclass `ParseResource`.
-    def self.all
-      Query.new(self).all
-    end
-
-    # Find the first object. Fairly random, not based on any specific condition.
-    #
-    def self.first
-      Query.new(self).limit(1).first
-    end
-
-    # Limits the number of objects returned
-    #
-    def self.limit(n)
-      Query.new(self).limit(n)
-    end
-
-    # Skip the number of objects
-    #
-    def self.skip(n)
-      Query.new(self).skip(n)
-    end
-    
-    #def self.order(attribute)
-    #  Query.new(self).order(attribute)
-    #end
 
     # Create a ParseResource::Base object.
     #
