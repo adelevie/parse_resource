f5ad20ad15c101936577745361292d6c4dc54e48
Sol Irvine <sol@densevector.com>
2013-09-06 17:11:45 -0400

    working on PFRole REST API calls

--- lib/parse_resource/parse_role.rb
+++ lib/parse_resource/parse_role.rb
@@ -8,4 +8,21 @@ class ParseRole < ParseResource::Base
   def users
     val = User.related_to(self, :users)
   end
+
+  def self.add_user(user_id)
+    base_uri   = "https://api.parse.com/1/roles"
+    app_id     = settings['app_id']
+    master_key = settings['master_key']
+    resource = RestClient::Resource.new(base_uri, app_id, master_key)
+    
+    begin
+      resp = resource.put(:params => {:id => self.objectId, :users => {"__op" => "AddRelation", "objects" => [{ "__type" => "Pointer", "className" => "_User", "objectId" => user.objectId}]}})
+      resp
+    rescue
+      debug 
+      false
+    end
+    
+  end
+
 end
